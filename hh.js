b0d56580f5f553ad61520547e5731e74b93d9030
vivekbharatha <vivek.bharatha@gmail.com>
2017-04-30 17:18:23 +0530

    Basic dialog done and linked to Recast.aio

--- hh.js
+++ hh.js
@@ -1,5 +1,32 @@
 const restify = require('restify');
 const Builder = require('botbuilder');
-
+const Recast = require('recastai');
+require('dotenv').config();
 const PORT = process.env.PORT || 8080;
-const server = restify.createServer().listen(PORT);
+// Connection to Microsoft Bot Framework
+const connector = new Builder.ChatConnector({
+  appId: process.env.APP_ID,
+  appPassword: process.env.APP_PASSWORD
+});
+// Bot config
+const bot = new Builder.UniversalBot(connector);
+// Recast.ai config
+const recastClient = new Recast.request(process.env.RECAST);
+// Event when message received
+bot.dialog('/', (session) => {
+  // Pass to Recast.ai
+  recastClient.analyseText(session.message.text)
+    .then(res => {
+      const intent = res.intent();
+      if (intent.slug === 'core') {
+        session.send('Sorry to hear about that! I understand, please tell about her present mood');
+      } else {
+        session.send('Sorry, I need to train  myself am not working perfectly ! :(');
+      }
+    })
+    .catch(() => session.send('I need some help right now :( Talk to me later!'));
+});
+// Server init
+const server = restify.createServer();
+server.listen(PORT);
+server.post('/', connector.listen());
